f743bcda86dfcb671a6df8210b075dffc64c4409
Daniil Gnidenko <dagnidenko@gmail.com>
2025-08-31 14:14:58 +0300

    Добавлено логирование для services.py

--- src/services.py
+++ src/services.py
@@ -1,23 +1,34 @@
 import json
+import os
 import re
 
 import pandas as pd
+from loguru import logger
+
+# Конфигурация логгера
+current_dir = os.path.dirname(os.path.abspath(__file__))
+log_dir = os.path.join(current_dir, "..", "logs")
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, "services.log")
+logger.add(sink=log_file, level="DEBUG")
 
 
 def analyze_cashback(data: list[dict], year: int, month: int) -> str:
     """Функция возвращает JSON с суммами по категориям кэшбэка в указанном месяце года"""
+    logger.debug(f"start analyzing cashback data for {year}/{month}. Input data len: {len(data)}")
     df = pd.DataFrame(data)
     df["Дата операции"] = pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S")
     df_filtered = df[(df["Дата операции"].dt.year == year) & (df["Дата операции"].dt.month == month)]
 
     result = df_filtered.groupby("Категория")["Бонусы (включая кэшбэк)"].sum().sort_values(ascending=False).to_dict()
-
+    logger.debug(f"Analyzed cashback len: {len(result)}")
     result_json = json.dumps(result, indent=4, ensure_ascii=False)
     return result_json
 
 
 def search_transactions(data: list[dict], search: str, scope: tuple = ("Описание", "Категория")) -> str:
     """Функция возвращает JSON со всеми транзакциями, найденными по паттерну в полях описание или категория"""
+    logger.debug(f"Start searching transactions for with pattern: '{search}'. Input data len: {len(data)}")
     pattern = re.compile(search)
     result = []
 
@@ -25,19 +36,17 @@ def search_transactions(data: list[dict], search: str, scope: tuple = ("Опис
         for transaction_key in transaction.keys():
             if transaction_key in scope:
                 value = transaction[transaction_key]
-                if isinstance(value, float):
-                    continue
-                if isinstance(value, (int, bool)):
+
+                if isinstance(value, (int, bool, float)):
                     value = str(value)
 
                 if isinstance(value, str) and pattern.search(value):
                     result.append(transaction)
                     break
-
+    logger.debug(f"Search result len: {len(result)}")
     return json.dumps(result, ensure_ascii=False, indent=4)
 
 
-# todo: сделать тесты
 def search_transactions_p2p(data: list[dict]) -> str:
     """Функция возвращает JSON со всеми транзакциями, которые относятся к переводам физ.лицам"""
     return search_transactions(data, r"[А-Я]{1}[а-я]+\s[А-Я]{1}\.", scope=("Описание",))
